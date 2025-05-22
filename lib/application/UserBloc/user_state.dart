4db02c201c2604a2f7c455762ecb37cc8b77f048
asna.a <asna.a@geekywolf.com>
2025-05-22 19:08:44 +0530

    null handling

--- lib/application/UserBloc/user_state.dart
+++ lib/application/UserBloc/user_state.dart
@@ -2,8 +2,8 @@ part of 'user_bloc.dart';
 
 @generateStates
 abstract class _$$UserState {
-  final bool? isLoading = false;
-  final bool? isLoaded = false;
+  final bool isLoading = false;
+  final bool isLoaded = false;
   final List<UserModel> users = const [];
   final String? error = null;
 }
