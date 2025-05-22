b2b2a71e8b3b5b3beedeb596bf4323c84e03877c
asna.a <asna.a@geekywolf.com>
2025-05-22 15:56:03 +0530

    route

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,13 +1,14 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_sandbox/application/bloc/conuter_bloc.dart';
+import 'package:flutter_sandbox/application/UserServiceBloc/user_bloc.dart';
+import 'package:flutter_sandbox/application/bloc/counter_bloc/conuter_bloc.dart';
+import 'package:flutter_sandbox/application/bloc/example_bloc/example_bloc.dart';
 import 'package:flutter_sandbox/application/serviceBloc/service_bloc.dart';
-import 'package:flutter_sandbox/domain/ILoginRepo/ILoginRepo.dart';
 import 'package:flutter_sandbox/domain/core/dependency_injection/di_configuration.dart';
 import 'package:flutter_sandbox/presentation/home/home_screen.dart';
 import 'package:flutter_sandbox/presentation/profile/profile_screen.dart';
+import 'package:flutter_sandbox/presentation/screen1/screen1.dart';
+import 'package:flutter_sandbox/presentation/userList/users_list_screen.dart';
 
 abstract class MyRouter {
   static Route<dynamic> getRoutes(RouteSettings settings) {
@@ -21,7 +22,15 @@ abstract class MyRouter {
             );
           },
         );
-
+      case UsersListScreen.routeName:
+        return MaterialPageRoute(
+          builder: (context) {
+            return BlocProvider(
+              create: (context) => getIt<UserBloc>(),
+              child: UsersListScreen(),
+            );
+          },
+        );
       case ProfileScreen.routeName:
         var arguments = settings.arguments as String;
         return MaterialPageRoute(
@@ -29,6 +38,15 @@ abstract class MyRouter {
             return BlocProvider(
               create: (context) => getIt<ServiceBloc>(),
               child: ProfileScreen(name: arguments),
+            );
+          },
+        );
+      case Screen1.routeName:
+        return MaterialPageRoute(
+          builder: (context) {
+            return BlocProvider(
+              create: (context) => getIt<exampleBloc>(),
+              child: Screen1(),
             );
           },
         );
